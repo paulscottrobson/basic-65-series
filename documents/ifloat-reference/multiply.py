eff8fd0a1bed5aa3b2027369ac19b834b3c12a0c
Paul Robson <paul@robsons.org.uk>
2024-11-06 17:53:37 +0000

    Fixed up multiply

--- documents/ifloat-reference/multiply.py
+++ documents/ifloat-reference/multiply.py
@@ -30,36 +30,39 @@ class MultiplyOperation(BinaryOperation):
 			return IFloat(0)
 		exp = a.exponent + b.exponent  														# The new exponent, not allowing for shifts.
 		neg = a.isNegative != b.isNegative  												# The new sign.
-		r = self.shortMultiply(a,b)  														# Calculate a.b as if the exponents are zero.
-		r.exponent = r.exponent + exp  														# Add the shifts required to the new exponent
-		if r.mantissa != 0:  																# Apply new sign *only* if not zero.
-			r.isNegative = neg
-#		print("Result",r.get())
-		return r
+		a = self.shortMultiply(a,b)  														# Calculate a.b as if the exponents are zero.
+		a.exponent = a.exponent + exp  														# Add the shifts required to the new exponent
+		a.isNegative = neg
+
+		if a.mantissa == 0:  																# Minus zero fix.
+			a.isNegative = False
+#		print("Result",a.get())
+		return a
 	#
-	#		Short Multiply, calculates a.b assuming both are zero exponents, shifting if required.
+	#		Short Multiply, calculates r.b assuming both are zero exponents, shifting if required.
 	#
 	def shortMultiply(self,a,b):
-		r = IFloat(0) 																		# Set the result to zero
-		while b.mantissa != 0: 																# Typical shift-and-add multiply now ... almost
+		r = a  																				# Put A in R
+		a = IFloat(0) 																		# Set the result (A) to zero
+		while r.mantissa != 0: 																# Typical shift-and-add multiply now ... almost
 
 			mantissaOverflow = False 
-			if (b.mantissa & 1) != 0: 														# Bit add in b ?
-				r.mantissa = r.mantissa + a.mantissa  										# Add a to total
-				if (r.mantissa & 0x80000000) != 0: 											# Overflow.
-					r.mantissa = r.mantissa >> 1  											# So shift the result right and bump the exponent
-					r.exponent = r.exponent + 1
+			if (r.mantissa & 1) != 0: 														# Bit add in r ?
+				a.mantissa = a.mantissa + b.mantissa 	 									# Add b to total
+				if (a.mantissa & 0x80000000) != 0: 											# Overflow.
+					a.mantissa = a.mantissa >> 1  											# So shift the result right and bump the exponent
+					a.exponent = a.exponent + 1
 					mantissaOverflow = True 
 
 			if not mantissaOverflow:  														# Not necessary if mantissa has overflowed.
-				if (a.mantissa & 0x40000000) != 0: 											# We want to shift A left, but we can't.
-					r.mantissa = r.mantissa >> 1  											# So shift the result right and bump the exponent
-					r.exponent = r.exponent + 1		
+				if (b.mantissa & 0x40000000) != 0: 											# We want to shift B left, but we can't.
+					a.mantissa = a.mantissa >> 1  											# So shift the A right and bump the exponent
+					a.exponent = a.exponent + 1		
 				else:
-					a.mantissa = a.mantissa << 1   											# We can, if no normalising.
+					b.mantissa = b.mantissa << 1   											# We can, if no normalising.
 
-			b.mantissa = b.mantissa >> 1  													# Shift the B mantissa right
-		return r
+			r.mantissa = r.mantissa >> 1  													# Shift the R mantissa right
+		return a
 	#
 	#		Error percent allowed
 	#
